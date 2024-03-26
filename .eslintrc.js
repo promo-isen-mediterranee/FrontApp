c566b1e1450c7b272dd4883e0b31351dabc6dbd0
corentinthibaud <corentin.thibaud@isen.yncrea.fr>
2024-03-26 15:25:44 +0100

    feat: add storybook
    Signed-off-by: corentinthibaud <corentin.thibaud@isen.yncrea.fr>

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
     "extends": [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
-        "prettier"
+        "prettier",
+        "plugin:storybook/recommended"
     ],
     "overrides": [
         {
