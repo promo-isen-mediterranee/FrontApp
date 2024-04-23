d5ac0753600d67dcb74b87610e5c4cb38f9c8c69
corentinthibaud <corentin.thibaud@isen.yncrea.fr>
2024-04-23 08:55:16 +0200

    feat: implementing the log in page
    Signed-off-by: corentinthibaud <corentin.thibaud@isen.yncrea.fr>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgClass, NgOptimizedImage } from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgOptimizedImage, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'FrontApp';
 }
