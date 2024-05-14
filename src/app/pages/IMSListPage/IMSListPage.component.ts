5c3004b574e78775793a0da571d15841b0b363e5
alex <alex.olivier.2502@gmail.com>
2024-05-14 16:42:02 +0200

    Apport des items du backend
    Co-Authored-By: atlas sanchez <71553939+juan361@users.noreply.github.com>
    Co-Authored-By: baguera13 <dorian.bourdier@isen.yncrea.fr>

--- src/app/pages/IMSListPage/IMSListPage.component.ts
+++ src/app/pages/IMSListPage/IMSListPage.component.ts
@@ -1,15 +1,71 @@
-import { Component } from '@angular/core';
-import { ButtonComponent } from "../../components/button/button.component";
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { catchError, tap } from 'rxjs/operators';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { ButtonComponent } from '../../components/button/button.component';
+import { CommonModule } from '@angular/common';
+
+interface Item {
+  id: number;
+  item_id: {
+    id: number;
+    name: string;
+    category_id: {
+      id: number;
+      label: string;
+    }
+  };
+  location_id: {
+    id: number;
+    address: string;
+    city: string;
+    room: string;
+  };
+  quantity: number;
+}
 
 @Component({
-  selector: 'app-imslist-page',
+  selector: 'app-imslist-page',  
   standalone: true,
   imports: [
-    ButtonComponent
+    ButtonComponent,
+    CommonModule,
+    HttpClientModule,
+    RouterModule
   ],
   templateUrl: './IMSListPage.component.html',
   styleUrl: './IMSListPage.component.css'
-})
-export class IMSListPageComponent {
 
+})
+
+export class IMSListPageComponent implements OnInit {
+  items: Item[] = [];
+  constructor(private http: HttpClient) {}  
+  ngOnInit() {
+    this.http.get<Item[]>('http://localhost:5000/stock/item/getAll').pipe(
+      tap(data => {
+        this.items = data;
+      }),
+      catchError(error => {
+        console.error(error);
+        throw error;
+      })
+    ).subscribe();
+  }
+  
 }
+
+@NgModule({
+    imports: [
+      BrowserModule,
+      HttpClientModule,
+      CommonModule,
+      RouterModule
+    ],
+    providers: [],
+    schemas: [NO_ERRORS_SCHEMA]
+})
+export class AppModule { }
