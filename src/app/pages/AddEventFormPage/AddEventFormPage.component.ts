6d8ef8ce48065d59ba6284dce273cd7039ac2383
corentinthibaud <corentin.thibaud@isen.yncrea.fr>
2024-05-14 13:30:49 +0200

    fix: adding autocomplete on add event form page fix: rectifying some design flows
    Signed-off-by: corentinthibaud <corentin.thibaud@isen.yncrea.fr>

--- src/app/pages/AddEventFormPage/AddEventFormPage.component.ts
+++ src/app/pages/AddEventFormPage/AddEventFormPage.component.ts
@@ -9,9 +9,11 @@ import {
 } from "@angular/material/datepicker";
 import { MatNativeDateModule, MatOption } from "@angular/material/core";
 import { MatAutocomplete, MatAutocompleteTrigger } from "@angular/material/autocomplete";
-import { ReactiveFormsModule } from "@angular/forms";
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
 import { MatError, MatFormFieldModule } from "@angular/material/form-field";
 import { provideAnimations } from "@angular/platform-browser/animations";
+import { map, Observable, startWith } from "rxjs";
+import { AsyncPipe } from "@angular/common";
 
 @Component({
   selector: 'app-add-event-form-page',
@@ -32,7 +34,8 @@ import { provideAnimations } from "@angular/platform-browser/animations";
     MatOption,
     ReactiveFormsModule,
     MatAutocompleteTrigger,
-    MatError
+    MatError,
+    AsyncPipe
   ],
   providers: [
     MatDatepickerModule,
@@ -44,8 +47,21 @@ import { provideAnimations } from "@angular/platform-browser/animations";
   styleUrl: './AddEventFormPage.component.css'
 })
 export class AddEventFormPageComponent {
-
+  public addressControl = new FormControl();
   protected options: string[] = ['option 1', 'option 2', 'option 3'];
+  filteredOption: Observable<string[]> = new Observable();
+
+  ngOnInit() {
+    this.filteredOption = this.addressControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || ''))
+    )
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
 
   public closeDatepicker(datepicker: MatDatepicker<Date>): void {
     console.log('closing datepicker')
--- src/app/pages/AdminPage/AdminPage.component.ts
+++ src/app/pages/AdminPage/AdminPage.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../../components/button/button.component";
+import { NgOptimizedImage } from "@angular/common";
 
 @Component({
   selector: 'app-AdminPage',
   standalone: true,
   imports: [
-    ButtonComponent
+    ButtonComponent,
+    NgOptimizedImage
   ],
   templateUrl: './AdminPage.component.html',
   styleUrl: './AdminPage.component.css'
