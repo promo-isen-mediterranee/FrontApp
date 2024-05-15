d745ac10ab98a298d4a4bcf0354c342aad8e20b5
Marc ETAVARD <rakushain@gmail.com>
2024-05-15 16:20:52 +0200

    Fix AddEvent hmtl

--- src/app/pages/AddEventFormPage/AddEventFormPage.component.ts
+++ src/app/pages/AddEventFormPage/AddEventFormPage.component.ts
@@ -26,7 +26,7 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { AsyncPipe } from "@angular/common";
+import { AsyncPipe } from '@angular/common';
 
 export interface Location {
   address: string;
@@ -53,7 +53,7 @@ export interface Location {
     ReactiveFormsModule,
     MatAutocompleteTrigger,
     MatError,
-    AsyncPipe
+    AsyncPipe,
   ],
   providers: [
     MatDatepickerModule,
