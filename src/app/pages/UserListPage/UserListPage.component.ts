62f8d05fd4a0e23249a7d542d809b72e52040ff1
Atlas <jgsan360@gmail.com>
2024-05-17 15:24:38 +0200

    ajout des fonctionnalités modifier et supprimer (front)

--- src/app/pages/UserListPage/UserListPage.component.ts
+++ src/app/pages/UserListPage/UserListPage.component.ts
@@ -1,15 +1,39 @@
 import { Component } from '@angular/core';
+//import { catchError, tap } from 'rxjs/operators';
+//import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ButtonComponent } from "../../components/button/button.component";
+import { environment } from "../../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+interface  User {
+  user:{
+    username:string;
+    email:string;
+  };
+  role:{
+    id:number;
+    label:string;
+  };
+}
 
 @Component({
   selector: 'app-UserListPage',
   standalone: true,
   imports: [
-    ButtonComponent
+    ButtonComponent,
+    MatIconModule,
+    MatButtonModule
   ],
   templateUrl: './UserListPage.component.html',
   styleUrl: './UserListPage.component.css'
 })
 export class UserListPageComponent {
-
+  toggleActions(event: MouseEvent): void {
+    const fileElement = (event.currentTarget as HTMLElement).closest('.file');
+    if (fileElement) {
+      fileElement.classList.toggle('show-actions');
+    }
+  }
 }
+
