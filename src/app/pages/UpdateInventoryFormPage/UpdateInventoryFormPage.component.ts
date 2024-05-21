5d0c49d22f1ae62430e93c1b3523ea23a7bd77c4
alex <alex.olivier.2502@gmail.com>
2024-05-21 12:14:29 +0200

    Passage ng test

--- src/app/pages/UpdateInventoryFormPage/UpdateInventoryFormPage.component.ts
+++ src/app/pages/UpdateInventoryFormPage/UpdateInventoryFormPage.component.ts
@@ -15,6 +15,26 @@ import { environment } from '../../../environments/environment';
 import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+
+interface Item {
+  id: number;
+  item_id: {
+    id: number;
+    name: string;
+    category_id: {
+      id: number;
+      label: string;
+    };
+  };
+  location_id: {
+    id: number;
+    address: string;
+    city: string;
+    room: string;
+  };
+  quantity: number;
+}
+
 @Component({
   selector: 'app-update-inventory-form-page',
   standalone: true,
@@ -36,7 +56,25 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrl: './UpdateInventoryFormPage.component.css',
 })
 export class UpdateInventoryFormPageComponent {
-  selectedItem: any = {};
+  selectedItem: Item = {
+    id: 0,
+    item_id: {
+      id: 0,
+      name: '',
+      category_id: {
+        id: 0,
+        label: ''
+      }
+    },
+    location_id: {
+      id: 0,
+      address: '',
+      city: '',
+      room: ''
+    },
+    quantity: 0
+  };
+
   private apiUrl = environment.apiStockUrl;
   Location: string = '';
   constructor(
@@ -57,10 +95,10 @@ export class UpdateInventoryFormPageComponent {
     );
     console.log(this.selectedItem);
     const ItemData = new URLSearchParams();
-    ItemData.set('name', this.selectedItem.item_id.name);
-    ItemData.set('quantity', this.selectedItem.quantity);
-    ItemData.set('category', this.selectedItem.item_id.category_id.label);
-    ItemData.set('location.id', this.selectedItem.location_id.id);
+    ItemData.set('name', this.selectedItem?.item_id?.name ?? '');
+    ItemData.set('quantity', String(this.selectedItem?.quantity ?? 0));
+    ItemData.set('category', this.selectedItem?.item_id.category_id.label ?? '');
+    ItemData.set('location.id', String(this.selectedItem?.location_id.id ?? ''));
     this.http
       .put( this.apiUrl + 'item/' + this.selectedItem.item_id.id + '/' + this.selectedItem.location_id.id , ItemData, {
         headers,
